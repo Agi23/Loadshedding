eff5ae44d7dc4ef74ef1dcbc638bb5e2f0353433
Agoritsa Spirakis <agoritsa.spirakis@gmail.com>
2020-03-01 19:01:38 +0200

    Basic operations counter for array that prints to terminal

--- src/LSArrayApp.java
+++ src/LSArrayApp.java
@@ -5,19 +5,19 @@ public class LSArrayApp {
    
     public static void main(String[] args) {
         LSArray ArrayData;
-        System.out.println("hello");
         
         if (args.length == 0)
             ArrayData = new LSArray();
         else{
             //check if correct format
-            if (args.length == 3)
+            if (args.length == 3){
                 ArrayData = new LSArray(args[0], args[1], args[2]);
-                
+                System.out.println("No of operations: " + ArrayData.getCounter());
+            } 
             else{
                 System.out.println("Error: incorrect format");}
         }
-            
+         
     } 
      
 }
