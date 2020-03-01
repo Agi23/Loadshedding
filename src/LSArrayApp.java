1c20836f272c3a32233b6b9224f197ea029085aa
Agoritsa Spirakis <agoritsa.spirakis@gmail.com>
2020-03-01 18:47:55 +0200

    PrintAreas search method working

--- src/LSArrayApp.java
+++ src/LSArrayApp.java
@@ -2,13 +2,22 @@
 
 public class LSArrayApp {
     
-    
+   
     public static void main(String[] args) {
         LSArray ArrayData;
         System.out.println("hello");
-        ArrayData = new LSArray();
-
         
+        if (args.length == 0)
+            ArrayData = new LSArray();
+        else{
+            //check if correct format
+            if (args.length == 3)
+                ArrayData = new LSArray(args[0], args[1], args[2]);
+                
+            else{
+                System.out.println("Error: incorrect format");}
+        }
+            
     } 
      
 }
